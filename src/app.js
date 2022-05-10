4dbf2b1750b64b036bc2931b2b7cc568eaf46a6e
PedroParraPerez <pedroparra06091994@gmail.com>
2022-05-10 00:18:18 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -4,26 +4,26 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-let domaingenerator = document.querySelector("#domain");
+
 window.onload = function() {};
 
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "telecom", "supercabinet"];
 let ext = [".com", ".net"];
-let frases = [];
+let words = [];
 
-for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-    for (let z = 0; z < noun.length; z++) {
-      for (let y = 0; y < ext.length; y++) {
-        let domain = `${pronoun[i] + adj[j] + noun[z] + ext[y]}`;
+const DomainGenerator = () => {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let z = 0; z < noun.length; z++) {
+        for (let y = 0; y < ext.length; y++) {
+          let domain = `${pronoun[i] + adj[j] + noun[z] + ext[y]}`;
 
-        domaingenerator.innerHTML = document.write(
-          `<li class="text-center">${domain}</li>`
-        );
+          document.write(`<li class="text-center mt-2">${domain}</li>`);
+        }
       }
     }
   }
-}
-
+};
+DomainGenerator();
