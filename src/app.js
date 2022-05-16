b92384f375a505fa13b1f765d313fbbdd7124e63
PedroParraPerez <pedroparra06091994@gmail.com>
2022-05-16 06:17:22 +0000

    refactor

--- src/app.js
+++ src/app.js
@@ -11,19 +11,18 @@ let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "telecom", "supercabinet"];
 let ext = [".com", ".net"];
-let words = [];
+let domain = "";
 
 const DomainGenerator = () => {
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let z = 0; z < noun.length; z++) {
         for (let y = 0; y < ext.length; y++) {
-          let domain = `${pronoun[i] + adj[j] + noun[z] + ext[y]}`;
-
-          document.write(`<li class="text-center mt-2">${domain}</li>`);
+          domain += `<li>${pronoun[i] + adj[j] + noun[z] + ext[y]}</li>`;
         }
       }
     }
   }
+  return domain;
 };
-DomainGenerator();
+document.querySelector(".list").innerHTML = DomainGenerator();
